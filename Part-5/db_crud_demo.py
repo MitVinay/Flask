90cf28c7b06d4c9facf13e4a4e15baaf2f6b4451
MitVinay <vm05011996@gmail.com>
2024-06-27 04:56:59 +1000

    Made a new data base

---
+++ Part-5/db_crud_demo.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+"""
+The url is the link of the database we want to use. please
+refer for more information in the link below
+https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
+
+"""
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///employees_db.db" 
+# to hide the warning etc given by SQLite, this is optional
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
+
+"""
+Lets define a Table employee,
+db.Model is passed to defined it as table
+"""
+"""
+This is the builtin method of the class, So we are modifying  the 
+output.
+Intially if we just run class it displays its memory location with the
+given below we are improving the ouput.
+"""
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"Employee({self.name}, {self.age}, {self.email})"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
