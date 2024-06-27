75ecd40d0ec73ea06d4cbad4ec6a75d780461fe8
MitVinay <vm05011996@gmail.com>
2024-06-27 22:27:41 +1000

    Working on the SQLite

--- Part-5/notes.py
+++ Part-5/notes.py
@@ -10,5 +10,8 @@ Example:
 
 obj1 = Table()
 
+The Database will use SQLite Database. It is serverless. which means the data 
+will be simply stored in the file.
 
+Packages: pip install flask-sqlalchemy
 """
